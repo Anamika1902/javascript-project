daf847c0b2b05663a144b31a89bf44a40547adab
Anamika1902 <95669760+Anamika1902@users.noreply.github.com>
2025-02-14 09:57:51 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const city="thane"
+let accountId=123;
+// city="mulund" //not allowed
+var accoundEmail="anamikarao@gmail.com";
+let user;
+// accountId=456
+accoundEmail="annaA@gmail.com";
+
+var user2;
+console.table([accountId,accoundEmail,city,user ,user2]);
